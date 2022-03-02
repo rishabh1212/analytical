4d60fde2c51e8f28094de774f4a7af1c968607e8
Rishabh Chavhan <rishabh6417@gmail.com>
2022-03-03 01:53:22 +0900

    Amplify auth

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig)
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
